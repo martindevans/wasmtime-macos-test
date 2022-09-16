e2fa759914a7504d04945b47fa1686f3a8ea099e
Martin Evans <martindevans@gmail.com>
2022-09-16 19:47:04 +0100

    Copied program.cs into a test

--- wasmtime-dotnet/tests/TrapTests.cs
+++ wasmtime-dotnet/tests/TrapTests.cs
@@ -62,6 +62,64 @@ namespace Wasmtime.Tests
                 .Throw<TrapException>();
         }
 
+        [Fact]
+        public void CopiedFromProgram()
+        {
+            const string WAT = @"(module
+                (export ""run""(func $run))
+                (export ""run_div_zero""(func $run_div_zero))
+                (export ""run_div_zero_with_result""(func $run_div_zero_with_result))
+                (export ""run_stack_overflow""(func $run_stack_overflow))
+
+                (func $run
+                    (call $first)
+                )
+                (func $first
+                    (call $second)
+                )
+                (func $second
+                    (call $third)
+                )
+                (func $third
+                    unreachable
+                )
+
+                (func $run_div_zero_with_result(result i32)
+                    (i32.const 1)
+                    (i32.const 0)
+                    (i32.div_s)
+                )
+                
+                (func $run_div_zero
+                    (call $run_div_zero_with_result)
+                    (drop)
+                )
+                
+                (func $run_stack_overflow
+                    (call $run_stack_overflow)
+                )
+            )";
+
+
+            var config =  new Config().WithReferenceTypes(true);
+            var engine = new Engine(config);
+            var module = Module.FromText(engine, "name", WAT);
+            var linker = new Linker(engine);
+            var store = new Store(engine);
+            var instance = linker.Instantiate(store, module);
+            var run = instance.GetAction("run_stack_overflow")!;
+
+            try
+            {
+                run();
+                throw new NotImplementedException("No trap :(");
+            }
+            catch (TrapException ex)
+            {
+                Console.WriteLine("TRAPPED OK. FRAMES: " + (ex.Frames?.Count ?? 0));
+            }
+        }
+
         public void Dispose()
         {
             Store.Dispose();
